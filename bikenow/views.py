827835d9910f681f5c7904c5e7c350a3a85a3a96
alec <stahelbling1@ppsstudents.net>
2016-04-20 18:09:30 -0400

    added the views

--- bikenow/views.py
+++ bikenow/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render_to_response
 
 def index(request):
 	return render_to_response('index.html')
+
+def club(request):
+	return render_to_response('club.html')
+	
+def events(request):
+	return render_to_response('events.html')
+	
+def friends(request):
+	return render_to_response('friends.html')
+	
+def home(request):
+	return render_to_response('home.html')
+
+def profile(request):
+	return render_to_response('profile.html')
